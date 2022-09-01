1efe20b5bdda5730a69965fe773f5a6dc74087c9
igormachadogomes <imgomes00@gmail.com>
2022-09-01 20:47:58 +0000

    arquivos atualizados em develop

--- gasolina.py
+++ gasolina.py
@@ -4,8 +4,7 @@ import seaborn as sns
 data = pd.read_csv("gasolina.csv")
 with sns.axes_style('whitegrid'):
  grafico = sns.lineplot(data=data, x="dia", y="venda", palette="pastel")
- grafico.set(title='Preço da gasolina por dia', xlabel='Dia', ylabel='Preço (USD)')
+ grafico.set(title='Preço da gasolina/dia', xlabel='Dia do Mês', ylabel='Preço em dólares')
 
 fig = grafico.get_figure()
 fig.savefig('gasolina.png')
-
